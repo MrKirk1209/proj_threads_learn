7e49b459fd61f18fd5e14dab2880839e6d556137
Kirill <k.godov@list.ru>
2025-04-27 17:25:23 +0500

    new db

---
+++ ReadMe.md
@@ -0,0 +1,5 @@
+Находимся в ../proj_threads_learn
+Создание миграции
+alembic revision --autogenerate -m "Name migration"
+Установка миграции 
+alembic upgrade head
--- app/models/models.py
+++ app/models/models.py
@@ -4,40 +4,59 @@ from app.database import Base, str_uniq, int_pk, str_null_true
 from datetime import date
 
 
-# создаем модель таблицы студентов
-class Student(Base):
+# Таблица постов
+class Post(Base):
     id: Mapped[int_pk]
-    phone_number: Mapped[str_uniq]
-    first_name: Mapped[str]
-    last_name: Mapped[str]
-    date_of_birth: Mapped[date]
-    email: Mapped[str_uniq]
-    address: Mapped[str] = mapped_column(Text, nullable=False)
-    enrollment_year: Mapped[int]
-    course: Mapped[int]
-    special_notes: Mapped[str_null_true]
-    major_id: Mapped[int] = mapped_column(ForeignKey("majors.id"), nullable=False)
+    title: Mapped[str_uniq] 
+    content: Mapped[str] = mapped_column(Text, nullable=False)
+    image_url: Mapped[str]
+    author_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    author: Mapped["User"] = relationship("User", back_populates="posts")
+    # address: Mapped[str] 
+    # enrollment_year: Mapped[int]
+    # course: Mapped[int]
+    # special_notes: Mapped[str_null_true]
 
-    major: Mapped["Major"] = relationship("Major", back_populates="students")
+    # def __str__(self):
+    #     return (f"{self.__class__.__name__}(id={self.id}, "
+    #             f"first_name={self.first_name!r},"
+    #             f"last_name={self.last_name!r})")
 
-    def __str__(self):
-        return (f"{self.__class__.__name__}(id={self.id}, "
-                f"first_name={self.first_name!r},"
-                f"last_name={self.last_name!r})")
+    def __repr__(self):
+        return str(self)
+
+# Таблица Тредов
+class Thread(Base):
+    id: Mapped[int_pk]
+    content: Mapped[str] = mapped_column(Text, nullable=False)
+    creator_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    parent_id: Mapped[int] = mapped_column(ForeignKey("threads.id"), nullable=True)
+    image_url: Mapped[str]
+    post_id: Mapped[int] = mapped_column(ForeignKey("posts.id"), nullable=False)
+    post: Mapped["Post"] = relationship("Post", back_populates="threads")
+    user: Mapped["User"] = relationship("User", back_populates="threads")
+    parent: Mapped["Thread"] = relationship("Thread", back_populates="threads")
+
+    # count_students: Mapped[int] = mapped_column(server_default=text('0'))
+
+    # def __str__(self):
+    #     return f"{self.__class__.__name__}(id={self.id}, major_name={self.major_name!r})"
 
     def __repr__(self):
         return str(self)
 
 
-# создаем модель таблицы факультетов (majors)
-class Major(Base):
+# Таблица Пользователей
+class User(Base):
     id: Mapped[int_pk]
-    major_name: Mapped[str_uniq]
-    major_description: Mapped[str_null_true]
-    count_students: Mapped[int] = mapped_column(server_default=text('0'))
-
-    def __str__(self):
-        return f"{self.__class__.__name__}(id={self.id}, major_name={self.major_name!r})"
+    email: Mapped[str_uniq] 
+    user_name: Mapped[str_uniq] 
+    user_password: Mapped[str] 
+    role_id: Mapped[int] = mapped_column(ForeignKey("roles.id"), nullable=False)
+    role: Mapped["Role"] = relationship("Role", back_populates="users")
+class Role(Base):
+    id: Mapped[int_pk]
+    role_name: Mapped[str_uniq] 
 
     def __repr__(self):
         return str(self)
